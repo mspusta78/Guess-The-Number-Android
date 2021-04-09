08e174b9882957e1491a7e7204251c7170833297
Martin Spusta <lucas.spusta@gmail.com>
2021-04-08 20:16:00 -0400

    added on click and on long press

--- app/src/main/java/com/yourwebsite/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/yourwebsite/guessthenumber/MainActivity.kt
@@ -2,10 +2,29 @@ package com.yourwebsite.guessthenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.yourwebsite.guessthenumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnPressMe.setOnClickListener {
+            count += 1
+            binding.btnPressMe.text = "I was Pressed $count times."
+        }
+
+        binding.btnPressMe.setOnLongClickListener {
+            count += 1
+            binding.btnPressMe.text = "I was Long Pressed $count times."
+            true
+        }
+
     }
 }
